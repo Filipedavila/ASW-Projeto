8f5f8ca5fca7f1b87c04859a40b044eade4259da
filipe <filipedavila6@gmail.com>
2022-05-02 18:34:19 +0100

    perfil voluntario update

--- perfil_voluntario.php
+++ perfil_voluntario.php
@@ -22,87 +22,47 @@ if(isset($_SESSION['id'])){
 <article  class="form-group  justify-content-center">
 
     <div class="justify-content-center">
-
-
-
-            <!--  verificar se esta em falta o nome -->
+            <div class= "row">
+                <div class="col">
+                    <p>Imagem de Perfil: <?php echo $data[0]['imgPath'] ?></p>
+                </div>
+            </div>
             <div class=" row">
                 <div class="col">
-                  Nome:<?php echo $data[0]['nome'] ?>
+                    <p>Nome: <?php echo $data[0]['nome'] ?></p>
                 </div>
-              
+                <div class="col">
+                <p>Gênero: <?php echo $data[0]['genero'] ?></p>
+                </div>
+                <div class="col">
+                <p>Data de nascimento: <?php echo $data[0]['dob'] ?></p>
+                </div>
             </div>
             <div class="row">
 
                 <div class="col">
-                 Telefone:<?php echo $data[0]['telefone'] ?>
+                 <p>Telefone: <?php echo $data[0]['telefone'] ?></p>
+                </div>
+                <div class="col">
+                 <p>Carta de certidão: <?php echo $data[0]['cc'] ?></p>
+                </div>
+                <div class="col">
+                 <p>Carta de condução: <?php echo $data[0]['carta_conducao'] ?></p>
                 </div>
             </div>
-
             <div class="row">
                
                 <div class="col">
-
-                    Morada:
-                        <?php echo $data[0]['morada'] ?>
-                    
-                    </div>
-
-
-            </div>
-
-
-
-            <div class="row">
-                <div class="col">
-
-                    Nome Responsavel:<?php echo $data[0]['nome_contacto'] ?> 
-
-
+                    <p>Distrito: <?php echo $data[0]['nome_distrito'] ?></p>
                 </div>
-                <div class="col">
-                   Contacto Responsavel:
-                       <?php echo $data[0]['n_contacto'] ?>
-                           </div>
-
+                <div class="col"> 
+                    <p>Freguesia: <?php echo $data[0]['nome_freguesia'] ?></p>
+                </div>
+                <div class="col"> 
+                    <p>Freguesia: <?php echo $data[0]['nome_concelho'] ?></p>
+                </div>
             </div>
-
-
     </div>
 
-
-    <div class="row">
-        <div class="col">
-
-            <label for="tipo_inst">Tipo de Instituição: <?php echo $data[0]['tipo_inst'] ?>
-        </div>
-
-    </div>
-
-
-    <div class="row">
-
-        <div class="col">
-
-        <div class="callout"> <h1> Descrição </h1> 
-        <?php echo $data[0]['descricao'] ?></div>
-
-
-
-
-
-        </div>
-    </div>
-    
-    </div>
-    </div>
-
-
-    </div>
-    </form>
-
-
-
-    </div>
 
 </article>
