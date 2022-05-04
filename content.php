e3cd3713ca8014bd2eaa487756bf36cfbe907650
filipe <filipedavila6@gmail.com>
2022-05-04 17:12:47 +0100

    update content

--- content.php
+++ content.php
@@ -3,9 +3,9 @@
     <article class="row mt-5">
     <div class="col ml-5">
         
-    <h1>Index</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <h1>REFOOD</h1>
+    <p>o Movimento REFOOD resgata toneladas de boa comida para eliminar o desperdício e alimentar pessoas necessitadas. Como? Construindo uma ponte humana entre o excesso e a necessidade. Para isso, convidamos toda a comunidade local a fazer parte de um movimento 100% voluntário que transforma, não só desperdício em nutrição, mas também as vidas de todos os envolvidos numa verdadeira economia circular e solidária - impulsionada pela boa vontade. O Movimento REFOOD mobiliza os recursos da comunidade e seu sentido de responsabilidade social e ambiental para cocriar uma sociedade mais justa, solidária e sustentável.
+
     </div>
     <div class="col">
         <div id="carouselExampleSlidesOnly" class="carousel carousel-dark slide" data-ride="carousel">
