f28f7b4221e360909f161a52df358b7fc1b4e479
tatiana-lopes <tatiana.lopes.a@outlook.com>
2022-05-24 09:24:09 +0100

    update web service

--- webServices/VolRecolhaDoacao/db_cli.php
+++ webServices/VolRecolhaDoacao/db_cli.php
@@ -3,8 +3,8 @@ require_once "../lib/nusoap.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // The request is using the POST method
-    if(isset($_POST['idVol']) && isset($_POST['utilizador']) && isset($_POST['password']) && isset($_POST['IDInst']) && isset($_POST['IDDoacao']) ){
-        $idVol = $_POST['idVol'];
+    if(isset($_POST['IDVol']) && isset($_POST['utilizador']) && isset($_POST['password']) && isset($_POST['IDInst']) && isset($_POST['IDDoacao']) ){
+        $IDVol = $_POST['IDVol'];
         $utilizador = $_POST['utilizador'];
         $password = $_POST['password'];
         $IDInst = $_POST['IDInst'];
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'http://appserver-01.alunos.di.fc.ul.pt/~asw09/ASW-Projeto/webServices/VolRecolhaDoacao/db_serv.php'
     );
     $error = $client->getError();
-    $result = $client->call('VolRecolhaDoacao', array('idVol' => $idVol, 'utilizador' => $utilizador, 'password' => $password , 'IDInst' => $IDInst, 'IDDoacao' => $IDDoacao));	//handle errors
+    $result = $client->call('VolRecolhaDoacao', array('IDVol' => $IDVol, 'utilizador' => $utilizador, 'password' => $password , 'IDInst' => $IDInst, 'IDDoacao' => $IDDoacao));	//handle errors
 
     echo "<h2>Pedido</h2>";
     echo "<pre>" . htmlspecialchars($client->request, ENT_QUOTES) . "</pre>";
--- webServices/VolRecolhaDoacao/db_serv.php
+++ webServices/VolRecolhaDoacao/db_serv.php
@@ -2,6 +2,10 @@
 require_once "../lib/nusoap.php";
 include SITE_ROOT . '/config/db_settings.php';
 
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set("display_errors", 1);
+ini_set("log_errors", 0); 
+
 function VolRecolhaDoacao($IDVol, $utilizador, $password, $IDInst, $IDDoacao)
 {	#$conn= getConnection();
 	$dbhost="appserver-01.alunos.di.fc.ul.pt";
@@ -11,15 +15,36 @@ function VolRecolhaDoacao($IDVol, $utilizador, $password, $IDInst, $IDDoacao)
 	//Verifica a ligação à BD
 	if(mysqli_connect_error()){die("Database connection failed:".mysqli_connect_error());}
 
-	$sql="SELECT * FROM Utilizador WHERE id ";
-	$result=mysqli_query($conn,$sql);
+	$utilizador="SELECT * FROM Utilizador WHERE id = {$IDVol} AND email LIKE {$utilizador}";
+	$result_utilizador=mysqli_query($conn,$utilizador);
+	$user = mysqli_fetch_assoc($result_utilizador);
+	echo $result_utilizador;
 
-	if (mysqli_num_rows($result) == 0) { 
+	//a parte do password nao ta a funcionar nao sei porque
+	$pass = password_verify($password, $user['pass']);
+	if ($pass == true){
+		//echo 'password true';
+	} else {
+		//echo 'password false';
+		//return 'Nao aceite';
+	}
+		
+	$inst = "SELECT * FROM Alimento WHERE inst_id = {$IDInst} AND id = {$IDDoacao} AND vol_id IS NULL";
+	$result_inst=mysqli_query($conn,$inst);
+
+	                                                                                //or pass == false
+	if (mysqli_num_rows($result_utilizador) == 0 or mysqli_num_rows($result_inst) == 0 ) { 
 		//sem result return nao aceite
+		//echo 'nao aceite';
 		return "Não aceite";
 	}
 
 	else {
+		$query = "UPDATE Alimento SET vol_id = {$IDVol} WHERE inst_id = {$IDInst} AND id = {$IDDoacao}";
+		$result=mysqli_query($conn,$query);
+		//echo 'aceite';
+		$query = "SELECT * From Alimento WHERE inst_id = {$IDInst} AND id = {$IDDoacao} AND vol_id = {$IDVol}";
+		$result=mysqli_query($conn,$query);
 
 		while($row=mysqli_fetch_array($result,MYSQLI_NUM)){
 			$html[]="<tr><td>".implode("</td><td>",$row)."</td></tr>";
@@ -27,8 +52,9 @@ function VolRecolhaDoacao($IDVol, $utilizador, $password, $IDInst, $IDDoacao)
 		$html="<table>".implode("\n",$html)."</table>";	
 		// echo $html;
 		mysqli_close($conn);
-		return $html;
-		//return "Aceite";
+
+		//return $html;
+		return "Aceite";
 	}
 
 }
@@ -36,7 +62,7 @@ function VolRecolhaDoacao($IDVol, $utilizador, $password, $IDInst, $IDDoacao)
 $server = new soap_server();
 $server->configureWSDL('cumpwsdl', 'urn:cumpwsdl');
 $server->register("VolRecolhaDoacao", // nome metodo
-array('idVol' => 'xsd:string', 'utilizador' => 'xsd:string', 'password' => 'xsd:string' , 'IDInst' => 'xsd:string', 'IDDoacao' => 'xsd:string'), //input
+array('IDVol' => 'xsd:string', 'utilizador' => 'xsd:string', 'password' => 'xsd:string' , 'IDInst' => 'xsd:string', 'IDDoacao' => 'xsd:string'), //input
 array('return' => 'xsd:string'), // output
 	'uri:cumpwsdl', // namespace
 	'urn:cumpwsdl#VolRecolhaDoacao', // SOAPAction
