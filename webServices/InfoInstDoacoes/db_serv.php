171a62f2c37ace1698f9963053af21cbd8ca10f4
tatiana-lopes <tatiana.lopes.a@outlook.com>
2022-05-25 20:06:04 +0100

    update

--- webServices/InfoInstDoacoes/db_serv.php
+++ webServices/InfoInstDoacoes/db_serv.php
@@ -4,13 +4,7 @@ include  '../../init.php';
 
 function InfoInstDoacoes($id)
 {   $conn = getConnection();
-	//$dbhost="appserver-01.alunos.di.fc.ul.pt";
-	//$dbuser="asw09";	$dbpass="aswgrupo09";	$dbname="asw09";
-	//Cria a ligação à BD
-	//$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-	//Verifica a ligação à BD
-	//if(mysqli_connect_error()){die("Database connection failed:".mysqli_connect_error());}
-
+	
 	$sql="SELECT Instituicao.tipo_inst, Utilizador.telefone, Distrito.nome_Distrito, Concelho.nome_Concelho ,Freguesia.nome_Freguesia, Alimento.tipo_doacao, Alimento.quantidade, Disponibilidade.hora_inicio, Disponibilidade.hora_fim, Disponibilidade.dia
 	FROM Utilizador, Instituicao, Distrito, Concelho, Freguesia, Disponibilidade, Alimento WHERE Utilizador.id = {$id} 
 	AND Utilizador.id = Instituicao.id_U 
@@ -28,26 +22,15 @@ function InfoInstDoacoes($id)
 		return "Sem dados";
 	} else {
 		
-		$response = preg_replace("/(</?)(\w+):([^>]*>)/", "$1$2$3", $result);
-		$xml = new SimpleXMLElement($response);
-		$body = $xml->xpath('//SBody')[0];
-		$array = json_decode(json_encode((array)$body), TRUE); 
-		print_r($array);
-		return $array;
-
-		/*
+		
 		while($row=mysqli_fetch_array($result,MYSQLI_NUM))
-
 			{
 				$html[]="<tr><td>".implode("</td><td>",$row)."</td></tr>";
 			}
 			$html="<table>".implode("\n",$html)."</table>";	
-			// echo $html;
 			mysqli_close($conn);
 			return $html;
-		*/
 	}
-
 }
 
 $server = new soap_server();
